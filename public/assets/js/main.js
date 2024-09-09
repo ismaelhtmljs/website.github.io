f528710d2ff02f0b80f1fb6bdccc313a6648709a
ismaelhtmljs <ismaelgq.88@gmail.com>
2024-09-08 22:52:45 -0500

    initial commit

---
+++ public/assets/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const BTN = document.getElementById('btn-submit');
+    
+    BTN.addEventListener('click', ()=> {
+        alert("todavia no hay una 'DataBase' donde guardar las cuentas logiadas, por ahora solo es haci");
+    });
+});
---
+++ public/assets/js/main_montañas.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const BTN = document.getElementById('titulo');
+
+    BTN.addEventListener('click', () => {
+        alert("esta es una pagina web de montañas famosas del mundo, espero que disfrute la experiencia ;D")
+    });
+});
